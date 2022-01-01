e976214f414521a5bf5788b7c03ae1e85461dd4a
akuma1999 <santoruy1999@gmail.com>
2022-01-01 14:36:03 +0700

    update some package

--- src/Page/Root.js
+++ src/Page/Root.js
@@ -1,30 +1,32 @@
-import { Route, Switch } from 'react-router-dom';
-import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 // import component
-import ShowCalendar from './ShowCalendar/ShowCalendar';
-import ListTask from './ListTask';
-import Loading from './loading';
+import ShowCalendar from "./ShowCalendar/ShowCalendar";
+import ListTask from "./ListTask";
+import Loading from "./loading";
 // import ListTask from './ListTask';
 // import ShowCalendar from './ShowCalendar/ShowCalendar';
 //
 
 // import style
-import './style.css';
+import "./style.css";
 //
 export default function Root() {
-  const [loading, setLoading] = React.useState(false);
-  return (
-    <>
-      <Switch>
-        <Route path='/tasks'>
-          <ListTask loading={(v) => setLoading(v)} />
-        </Route>
+	const [loading, setLoading] = React.useState(false);
+	return (
+		<>
+			<Routes>
+				<Route
+					path="/tasks"
+					element={<ListTask loading={(v) => setLoading(v)} />}
+				/>
 
-        <Route path='/'>
-          <ShowCalendar loading={(v) => setLoading(v)} />
-        </Route>
-      </Switch>
-      <Loading loading={loading} />
-    </>
-  );
+				<Route
+					path="/"
+					element={<ShowCalendar loading={(v) => setLoading(v)} />}
+				/>
+			</Routes>
+			<Loading loading={loading} />
+		</>
+	);
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './Service/Translate/i18next';
-import { Provider } from 'react-redux';
-import ConfigureStore from './Redux/ConfigureStore';
-import Root from './Page/Root';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./Service/Translate/i18next";
+import { Provider } from "react-redux";
+import ConfigureStore from "./Redux/ConfigureStore";
+import Root from "./Page/Root";
 
 const store = ConfigureStore();
 ReactDOM.render(
-  <Provider store={store}>
-    <I18nextProvider i18n={i18n}>
-      <Router>
-        <Root />
-      </Router>
-    </I18nextProvider>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<I18nextProvider i18n={i18n}>
+			<Router>
+				<Root />
+			</Router>
+		</I18nextProvider>
+		,
+	</Provider>,
+	document.getElementById("root")
 );
