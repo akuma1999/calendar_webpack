544412fae789efb0a83015f3cd4bfd5563cba967
akuma1999 <santoruy1999@gmail.com>
2022-01-04 18:44:29 +0700

    add handle change

--- src/Page/Root.js
+++ src/Page/Root.js
@@ -3,7 +3,9 @@ import React from "react";
 // import component
 import ShowCalendar from "./ShowCalendar/ShowCalendar";
 import ListTask from "./ListTask";
+import Sigin from "./Sigin";
 import Loading from "./loading";
+import Login from "./Login";
 // import ListTask from './ListTask';
 // import ShowCalendar from './ShowCalendar/ShowCalendar';
 //
@@ -21,12 +23,14 @@ export default function Root() {
 					element={<ListTask loading={(v) => setLoading(v)} />}
 				/>
 
+				<Route path="/login" element={<Login />} />
+				<Route path="/sigin" element={<Sigin />} />
 				<Route
 					path="/"
 					element={<ShowCalendar loading={(v) => setLoading(v)} />}
 				/>
 			</Routes>
-			<Loading loading={loading} />
+			{/* <Loading loading={loading} /> */}
 		</>
 	);
 }
--- src/Page/Sigin/index.js
+++ src/Page/Sigin/index.js
@@ -7,8 +7,10 @@ import "./style.scss";
 
 export default function LoginPage() {
 	const [data, setData] = React.useState({
+		name: "",
 		account: "",
 		pass: "",
+		re_pass: "",
 	});
 
 	const submit = () => {
@@ -19,10 +21,21 @@ export default function LoginPage() {
 		const { name, value } = e.target;
 		setData({ ...data, [name]: value });
 	};
+
 	return (
 		<div className="login-container">
 			<div>
-				<p>đăng nhập</p>
+				<p>đăng Ký</p>
+				<div>
+					<TextField
+						id="outlined-basic"
+						name="name"
+						onChange={(e) => textChange(e)}
+						label="Tên người dùng"
+						variant="outlined"
+						fullWidth={true}
+					/>
+				</div>
 				<div>
 					<TextField
 						id="outlined-basic"
@@ -44,11 +57,21 @@ export default function LoginPage() {
 					/>
 				</div>
 				<div>
+					<TextField
+						id="outlined-basic"
+						label="Lặp lại mật khẩu"
+						name="re_pass"
+						onChange={(e) => textChange(e)}
+						variant="outlined"
+						fullWidth={true}
+					/>
+				</div>
+				<div>
 					<Button
 						variant="contained"
 						color="primary"
-						fullWidth={true}
 						onClick={() => submit()}
+						fullWidth={true}
 					>
 						Gửi
 					</Button>
